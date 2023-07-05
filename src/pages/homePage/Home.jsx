fd0f8d4a30247a24babde62bb06cad07162eae55
RitikKumar202 <ritik2002july@gmail.com>
2023-07-05 23:50:09 +0530

    initial commit

---
+++ src/pages/homePage/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./home.scss";
+import Banner from "./banner/Banner";
+
+const Home = () => {
+  return (
+    <div className="homePage">
+      <Banner />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/homePage/banner/Banner.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+// import { useSelector } from "react-redux";
+import "./banner.scss";
+// import useFetch from "../../../hooks/useFetch";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import banner from "../../../assets/bg.jpg";
+
+const Banner = () => {
+  // const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  // const { url } = useSelector((state) => state.home);
+  // const { data, loading } = useFetch("/movie/trending");
+
+  // useEffect(() => {
+  //   const bg =
+  //     url.backdrop +
+  //     data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+  //   setBackground(bg);
+  // }, [data]);
+
+  const searchHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+
+  return (
+    <div className="banner">
+      <div className="backdrop-img">
+        <img src={banner} alt="" />
+      </div>
+
+      <div className="opacity-layer"></div>
+      <ContentWrapper>
+        <div className="banner-content">
+          <span className="title">
+            Welcome to <span>Cinepedia.</span>
+          </span>
+          <span className="subTitle">
+            Unlimited movies to explore? Search now.
+          </span>
+          <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search movies"
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyUp={searchHandler}
+            />
+            <button>Search</button>
+          </div>
+        </div>
+      </ContentWrapper>
+    </div>
+  );
+};
+
+export default Banner;
